c04a066f6aa95d83c352236a94b56f781a69cafb
Rodrigo Lopez <skinny229@gmail.com>
2019-03-11 16:12:47 -0400

    Removed limit switch and modified access modifiers

--- Deep Space Testing/src/main/java/frc/robot/ReactiveLEDs.java
+++ Deep Space Testing/src/main/java/frc/robot/ReactiveLEDs.java
@@ -9,39 +9,22 @@ import edu.wpi.first.wpilibj.Notifier;
 
 public class ReactiveLEDs {
 
-	I2C i2;
+	private I2C i2;
 
-	public ArrayList<String> cmdQueue = new ArrayList<String>();
-	DigitalInput limit = new DigitalInput(0);
-	DigitalInput limit2 = new DigitalInput(1);
+	private ArrayList<String> cmdQueue = new ArrayList<String>();
 
 	public class CommandCenter implements java.lang.Runnable {
 		boolean hasArduinoLink = false;
-		boolean lastInnerLimitState = false;
-		boolean lastOuterLimitState = false;
 
 		@Override
 		public void run() {
 			sendToArduino("Z");
-			updateLimitSwitches();
-			if (cmdQueue.size() == 0) {
-
-			} else {
-				sendToArduino(cmdQueue.get(0));
-				cmdQueue.remove(0);
-
-			}
+			//updateLimitSwitches();
+			if (cmdQueue.size() != 0)
+				sendToArduino(cmdQueue.remove(0));
 		}
 
 		public void updateLimitSwitches() {
-			if (limit.get() != lastInnerLimitState && limit.get()) {
-				addCmd("A");
-				lastInnerLimitState = !limit.get();
-			} else if (limit2.get() != lastInnerLimitState && limit2.get()) {
-				addCmd("B");
-			} else {
-				addCmd("C");
-			}
 		}
 		
 	}
@@ -50,28 +33,21 @@ public class ReactiveLEDs {
 
 	public ReactiveLEDs() {
 		i2 = new I2C(Port.kOnboard, 4);
-		// s2 = new SerialPort(0, Port.);
 		notify.startPeriodic(0.05);
 	}
 
-	public void sendToArduino(String input) {// writes to the arduino
-		char[] CharArray = input.toCharArray();// creates a char array from the
-												// input string
-		byte[] WriteData = new byte[CharArray.length];// creates a byte array
-														// from the char array
-		for (int i = 0; i < CharArray.length; i++) {// writes each byte to the
-													// arduino
-			WriteData[i] = (byte) CharArray[i];// adds the char elements to the
-												// byte array
-		}
-		// System.out.println("Data to Arduino" + WriteData);
-		// i2.transaction(WriteData, WriteData.length, null, 0);//sends each
-		// byte to arduino
-		// System.out.println("Sending...");
+	public void sendToArduino(String input) {
+		
+		char[] CharArray = input.toCharArray();
+		
+		byte[] WriteData = new byte[CharArray.length];
+		
+		for (int i = 0; i < CharArray.length; i++)										// arduino
+			WriteData[i] = (byte) CharArray[i];
 		i2.writeBulk(WriteData);
 	}
 
-	public void addCmd(String cmd) {
+	public void addCmd(char cmd) {
 		cmdQueue.add(cmd);
 	}
 
