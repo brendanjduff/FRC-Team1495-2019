521f31825cc3eb40524abd10fd82c7ebc961af59
Rodrigo Lopez <rkl5198@psu.edu>
2019-03-22 21:54:46 -0400

    .

--- CompetionCode_Update_2019/src/main/java/frc/robot/vision/VisionAlignRotate.java
+++ CompetionCode_Update_2019/src/main/java/frc/robot/vision/VisionAlignRotate.java
@@ -2,5 +2,15 @@ package frc.robot.vision;
 
 public class VisionAlignRotate {
 
-    
+    private static VisionAlignRotate instance = null;
+
+    public synchronized VisionAlignRotate getInstance()
+    {
+
+        if(instance == null)
+            instance = new VisionAlignRotate();
+
+    }
+
+
 }
