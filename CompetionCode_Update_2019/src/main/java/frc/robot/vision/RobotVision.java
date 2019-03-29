fda3ab9451fbd74587ae5dab4786ed3cb88b22e9
Rodrigo <skinny229@gmal.com>
2019-03-29 01:08:26 -0400

    Major Documentation and organization

--- CompetionCode_Update_2019/src/main/java/frc/robot/vision/RobotVision.java
+++ CompetionCode_Update_2019/src/main/java/frc/robot/vision/RobotVision.java
@@ -3,6 +3,7 @@ package frc.robot.vision;
 import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 
@@ -11,27 +12,21 @@ import static frc.robot.Robot.roboDrive;
 
 public class RobotVision {
 
-    //TODO add documentation
 
     private static RobotVision inst = null;
-
-    /*
-    *  Vision Settings
-    */
+    private NetworkTable ntVisionTable;
     private NetworkTable ntSettingsTable = null;
 
+    /*
+     *  Vision Settings for yaw differential
+     */
     private double targetPixel;
     private double focalLength;
     private double angleThreshold;
 
-
-
-
-
     /*
      * Vision Target Variables
      */
-    private NetworkTable ntVisionTable;
     private NetworkTableEntry ntVisionWidth;
     private NetworkTableEntry ntVisionCount;
     private NetworkTableEntry ntVisionAngle;
@@ -62,35 +57,38 @@ public class RobotVision {
     }
 
 
+    /*
+     * Should be run on periodic when needed
+     * */
     public void runPeriodicUpdate() {
         updateVisionTargetData();
-        //TODO Implement SmartDashboard data HERE
-        SmartDashboard.putBoolean("Vision Target Detection", hasTargets());
+        SmartDashboard.putBoolean("Does Vision Sees Targets", hasTargets());
     }
 
-    public boolean hasTargets() {
-        updateVisionTargetData();
-        return targetCount >= 1;
-    }
 
+    /*
+     *   Will run visionGuidance based on the mode selected (Call me WhilePressed)
+     * */
     public void runVisionGuidanceUpdate(int mode) {
-        if(!isNTReady())
+        if (!isNTReady())
             return;
         switch (mode) {
             case 0:
                 version1();
                 break;
             case 1:
-                yawDifferential();
+                visionYawDifferential();
                 break;
             default:
                 System.out.println("ERROR! Invalid Driving Mode");
         }
     }
 
+    /*
+       Sets up the NetworkTable Vision settings, allowing for some editing of parameters without reupload of code. MUST BE IMPLEMENTED IN ROBOT FOR VISION GUIDANCE TO WORK
+    */
     public void setNtVisionTable(NetworkTable ntVisionTable) {
-        if(this.ntVisionTable != null)
-        {
+        if (this.ntVisionTable != null) {
             System.out.println("WARNING! Vision table data attempted to be initialized twice! Exiting...");
             return;
         }
@@ -104,8 +102,11 @@ public class RobotVision {
         ntVisionCount = (this.ntVisionTable.getEntry("COUNT"));
     }
 
+    /*
+     *  Sets up the network table for the vision Data MUST BE IMPLEMENTED FOR VISION GUIDANCE TO WORK
+     * */
     public void setNtSettingsTable(NetworkTable ntSettingsTable) {
-        if(this.ntSettingsTable != null) {
+        if (this.ntSettingsTable != null) {
             System.out.println("WARNING! Vision table data attempted to be initialized twice! Exiting...");
             return;
         }
@@ -113,20 +114,20 @@ public class RobotVision {
         this.ntSettingsTable = ntSettingsTable;
 
 
-        this.ntSettingsTable.addEntryListener("FocalLength", (table, key, entry, value, flags) ->{
+        this.ntSettingsTable.addEntryListener("FocalLength", (table, key, entry, value, flags) -> {
             focalLength = entry.getValue().getDouble();
             System.out.println("Focal Length Value updated to: " + focalLength);
         }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
         this.ntSettingsTable.getEntry("FocalLength").setDouble(554.256258);
 
 
-        this.ntSettingsTable.addEntryListener("TargetPixel", (table, key ,entry, value, flags) ->{
+        this.ntSettingsTable.addEntryListener("TargetPixel", (table, key, entry, value, flags) -> {
             targetPixel = value.getDouble();
             System.out.println("TargetPixel Value updates to:" + targetPixel);
         }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
         this.ntSettingsTable.getEntry("TargetPixel").setDouble(319.5);
 
-        this.ntSettingsTable.addEntryListener("AngleThreshold", (table,key,entry,value,flags) -> {
+        this.ntSettingsTable.addEntryListener("AngleThreshold", (table, key, entry, value, flags) -> {
             angleThreshold = value.getDouble();
             System.out.println(key + " updated to: " + value.getDouble());
         }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
@@ -134,11 +135,119 @@ public class RobotVision {
 
     }
 
-    private boolean isNTReady()
-    {
+    /*
+        returns whether vision sees any targets
+    */
+    public boolean hasTargets() {
+        updateVisionTargetData();
+        return targetCount >= 1;
+    }
+
+    /*
+     * returns that status of tables readyness
+     * */
+    private boolean isNTReady() {
         return ntSettingsTable != null && ntVisionTable != null;
     }
 
+    /*
+     *  VisionYaw Mode, takes into account the change in angle needed to be aligned based on the X center
+     *  Adjustments done directly by vision
+     * */
+    private void visionYawDifferential() {
+        int targetIndexSelection = 0;
+        double xSpeed = 0.5;
+        double zSpeed = 0.0;
+        try {
+
+            if (targetCount > 1)
+                for (int i = 1; i < targetAngleArray.length; i++)
+                    if (targetAngleArray[i] < -40 || (targetAreaArray[targetIndexSelection] < targetAreaArray[i] && targetAngleArray[i] < -40))
+                        targetIndexSelection = i;
+                    else //noinspection ConstantConditions
+                        if (targetCount == 1 && targetAngleArray[0] > -40)
+                            targetIndexSelection = -1;
+                        else
+                            targetIndexSelection = -1;
+
+            VisionTarget target = updateVisionTarget(targetIndexSelection);
+
+
+            if (target != null) {
+
+                //works?
+                double angleToTarget = Math.atan((target.getX() - targetPixel) / focalLength) * (180.0 / Math.PI);
+
+                SmartDashboard.putNumber("Difference Angle To Target", angleToTarget);
+                SmartDashboard.putBoolean("YawDifferentialTargetFound", true);
+                //If Above threshold Compensate
+                if (Math.abs(angleToTarget) > angleThreshold) {
+                    xSpeed -=.1;
+                    zSpeed += angleToTarget < 0 ? -.3 : .3;
+                } else
+                    SmartDashboard.putNumber("Difference Angle To Target", 0);
+
+            } else
+                SmartDashboard.putBoolean("YawDifferentialTargetFound", false);
+
+            roboDrive.arcadeDrive(xSpeed, zSpeed);
+            if (encoderSpeed() == 0)
+                Robot.mExtender.setPiston(DoubleSolenoid.Value.kForward);
+
+            SmartDashboard.putBoolean("FailedCode", false);
+        } catch (Exception e) {
+            System.out.println("Something went wrong with Vision");
+            SmartDashboard.putBoolean("FailedCode", true);
+        }
+    }
+
+    /*
+        Returns the speed of the encoder in units, don't worry about unit conversion when implementing
+    * */
+    private double encoderSpeed() {
+        return 1.0;
+        //TODO IMPLEMENT ME
+    }
+
+
+    /*
+     *  Updates the vision data arrays
+     * */
+    private void updateVisionTargetData() {
+
+        if (ntVisionTable == null)
+            return;
+
+        targetCount = (int) ntVisionCount.getDouble(0);
+        targetWidthArray = ntVisionWidth.getDoubleArray(DEF_ARR);
+        targetAngleArray = ntVisionAngle.getDoubleArray(DEF_ARR);
+        targetAreaArray = ntVisionArea.getDoubleArray(DEF_ARR);
+        targetXArray = ntVisionX.getDoubleArray(DEF_ARR);
+        targetYArray = ntVisionY.getDoubleArray(DEF_ARR);
+        targetHeightArray = ntVisionHeight.getDoubleArray(DEF_ARR);
+    }
+
+    /*
+        Returns a VisionTarget Object based on index i of the target arrays
+
+        Int i = Index of the vision data arrays
+     */
+    private VisionTarget updateVisionTarget(int i) {
+        updateVisionTargetData();
+        if (i < 0)
+            return null;
+        return new VisionTarget(
+                i,
+                targetXArray[i],
+                targetYArray[i],
+                targetHeightArray[i],
+                targetWidthArray[i],
+                targetAreaArray[i],
+                targetAngleArray[i]
+        );
+    }
+
+    //Below is useless testing code, if needed call the vision guidance under mode 0
     private void version1() {
         double goodEnough = 3;
         double expectedXCentered = 213;
@@ -162,6 +271,7 @@ public class RobotVision {
             roboDrive.arcadeDrive(-.5, 0);
         }
     }
+
     private int getLowerAngle() {
         updateVisionTargetData();
         if (targetCount == 2) {
@@ -174,88 +284,5 @@ public class RobotVision {
         return -1;
     }
 
-    private void yawDifferential() {
-        int targetIndexSelection = 0;
-        try {
-
-            if (targetCount > 1)
-                for (int i = 1; i < targetAngleArray.length; i++)
-                    if (targetAngleArray[i] < -40 || (targetAreaArray[targetIndexSelection] < targetAreaArray[i] && targetAngleArray[i] < -40))
-                        targetIndexSelection = i;
-                    else //noinspection ConstantConditions
-                        if (targetCount == 1 && targetAngleArray[0] > -40)
-                            targetIndexSelection = -1;
-                        else
-                            targetIndexSelection = -1;
-
-            SmartDashboard.putNumber("Index Selection", targetIndexSelection);
-            VisionTarget target = updateRoboTarget(targetIndexSelection);
-
-
-            if (target != null) {
-                double angleToTarget = Math.atan((target.getX() - targetPixel) / focalLength)*(180.0/Math.PI);
-                SmartDashboard.putNumber("X current Target", target.getX());
-                SmartDashboard.putNumber("Difference", angleToTarget);
-                SmartDashboard.putBoolean("targetFound", true);
-                if (Math.abs(angleToTarget) > angleThreshold) {
-                    //TODO Implement roboDrive response if the angle is above the threshold
-                } else {
-                    roboDrive.arcadeDrive(.5, 0);
-
-                }
-
-            } else {
-                SmartDashboard.putBoolean("targetFound", false);
-                if (targetCount == 0/*TODO get accelerations for the class*/)
-                    roboDrive.arcadeDrive(.5, 0);
-                else {
-                    //TODO Release the thingy majig
-                    roboDrive.arcadeDrive(.5, 0);
-                }
-            }
-            if (encoderSpeed() == 0)
-                Robot.mExtender.TogglePiston();
-
-            SmartDashboard.putBoolean("FailedCode", false);
-        } catch (Exception e) {
-            System.out.println("Something went wrong with Vision");
-            SmartDashboard.putBoolean("FailedCode", true);
-        }
-    }
-
-    private double encoderSpeed()
-    {
-        return 1.0;
-    }
-
-
-    private void updateVisionTargetData() {
-
-        if(ntVisionTable == null)
-            return;
-
-        targetCount = (int) ntVisionCount.getDouble(0);
-        targetWidthArray = ntVisionWidth.getDoubleArray(DEF_ARR);
-        targetAngleArray = ntVisionAngle.getDoubleArray(DEF_ARR);
-        targetAreaArray = ntVisionArea.getDoubleArray(DEF_ARR);
-        targetXArray = ntVisionX.getDoubleArray(DEF_ARR);
-        targetYArray = ntVisionY.getDoubleArray(DEF_ARR);
-        targetHeightArray = ntVisionHeight.getDoubleArray(DEF_ARR);
-    }
-
-    private VisionTarget updateRoboTarget(int i) {
-        updateVisionTargetData();
-        if(i < 0)
-            return null;
-        return new VisionTarget(
-                i,
-                targetXArray[i],
-                targetYArray[i],
-                targetHeightArray[i],
-                targetWidthArray[i],
-                targetAreaArray[i],
-                targetAngleArray[i]
-        );
-    }
 
 }
